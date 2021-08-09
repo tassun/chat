0a98a9a741ffe4d9a45ab5891f78b339897ef303
tassun <tassun_oro@hotmail.com>
2021-08-09 11:12:02 +0700

    Delete client.js

--- client.js
+++
@@ -1,8 +0,0 @@
-var socket = require('socket.io-client')('http://127.0.0.1:9000');
-  
-socket.on('connect', function(){});
-socket.on('event', function(data){});
-socket.on('disconnect', function(){});
-socket.on('chat message', function(msg){
-	console.log(msg);
-});
